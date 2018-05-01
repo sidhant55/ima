430b0508f196e62a29b4a9a950f482a7e892b259
sid55 <sidhant.plb@gmail.com>
2018-05-01 21:00:58 +0530

    1-05-2018

--- quests/forms.py
+++ quests/forms.py
@@ -30,7 +30,7 @@ class Loginform(forms.Form):
     key=forms.CharField(max_length=100,widget=forms.TextInput(
             attrs={
                 'class':'input100',
-                'placeholder':'password',
+                'placeholder':'key',
             }
         ))
 
