9cf56c252ce9644ca47345921e3b30a1cde4a4d5
sid55 <sidhant.plb@gmail.com>
2018-07-31 09:39:54 +0530

    heroku

--- webapp/settings.py
+++ webapp/settings.py
@@ -147,10 +147,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# STATIC_URL = '/tmp/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'tmp'),]
+
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'tmp')
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/tmp/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tmp'),]
-
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'tmp')
-
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'tmp'),
+)
